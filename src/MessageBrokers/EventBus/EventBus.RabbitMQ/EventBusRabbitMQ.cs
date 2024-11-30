e4c3c115fa0862684eb67a72408a1876ffaec542
Melih Şimşek <Melihsimsek02@gmail.com>
2024-11-30 18:46:50 +0300

    Even bus infrastructure added

---
+++ src/MessageBrokers/EventBus/EventBus.RabbitMQ/EventBusRabbitMQ.cs
@@ -0,0 +1,180 @@
+﻿using EventBus.Base;
+using EventBus.Base.Events;
+using Newtonsoft.Json;
+using Polly;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using RabbitMQ.Client.Exceptions;
+using System.Net.Sockets;
+using System.Text;
+
+namespace EventBus.RabbitMQ
+{
+    public class EventBusRabbitMQ : BaseEventBus
+    {
+        RabbitMQPersistenceConnection persistentConnection;
+        private readonly IConnectionFactory connectionFactory;
+        private readonly IModel consumerChannel;
+
+        public EventBusRabbitMQ(EventBusConfig eventBusConfig,IServiceProvider serviceProvider)
+            :base(eventBusConfig,serviceProvider)
+        {
+            if (EventBusConfig.Connection is not null)
+            {
+                if (EventBusConfig.Connection is ConnectionFactory)
+                    connectionFactory = EventBusConfig.Connection as ConnectionFactory;
+                else
+                {
+                    var connJson = JsonConvert.SerializeObject(EventBusConfig.Connection, new JsonSerializerSettings()
+                    {
+                        ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+                    });
+                }
+            }
+            else
+                connectionFactory = new ConnectionFactory();
+
+            persistentConnection = new RabbitMQPersistenceConnection(connectionFactory, eventBusConfig.ConnectionRetryCount);
+            consumerChannel = CreateConsumerChannel();
+            SubsManager.OnEventRemoved += SubsManager_OnEventRemoved;
+        }
+
+        private void SubsManager_OnEventRemoved(object? sender, string e)
+        {
+            var eventName = ProcessEventName(e);
+
+            if(!persistentConnection.isConnected)
+            {
+                persistentConnection.TryConnect();
+            }
+
+            consumerChannel.QueueBind(
+                queue: eventName,
+                exchange: EventBusConfig.DefaultTopicName,
+                routingKey: eventName);
+
+            if(SubsManager.IsEmpty)
+            {
+                consumerChannel.Close();
+            }
+        }
+
+        public override void Publish(IntegrationEvent @event)
+        {
+            if(!persistentConnection.isConnected)
+            {
+                persistentConnection.TryConnect();
+            }
+
+            var policy = Policy.Handle<BrokerUnreachableException>()
+               .Or<SocketException>()
+               .WaitAndRetry(EventBusConfig.ConnectionRetryCount, retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)), (ex, time) =>
+               {
+
+               });
+
+            var eventName = @event.GetType().Name;
+            eventName = ProcessEventName(eventName);
+
+            consumerChannel.ExchangeDeclare(EventBusConfig.DefaultTopicName
+                ,type:"direct");
+            
+
+            var message = JsonConvert.SerializeObject(@event);
+            var body = Encoding.UTF8.GetBytes(message);
+
+            policy.Execute(() =>
+            {
+                var properties = consumerChannel.CreateBasicProperties();
+                properties.DeliveryMode = 2; 
+
+                consumerChannel.BasicPublish(
+                    exchange: EventBusConfig.DefaultTopicName,
+                    routingKey: eventName,
+                    mandatory: true,
+                    basicProperties: properties,
+                    body: body);
+            });
+        }
+
+
+        public override void Subscribe<T, TH>()
+        {
+            var eventName = typeof(T).Name;
+            eventName = ProcessEventName(eventName);
+
+            if(!SubsManager.HasSubscriptionsForEvent(eventName))
+            {
+                if(!persistentConnection.isConnected)
+                {
+                    persistentConnection.TryConnect();
+                }
+
+                consumerChannel.QueueDeclare(queue: GetSubName(eventName),
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null);
+
+                consumerChannel.QueueBind(queue: GetSubName(eventName),
+                    exchange: EventBusConfig.DefaultTopicName,
+                    routingKey: eventName);
+            }
+            SubsManager.AddSubscription<T,TH>();
+
+        }
+
+        public override void UnSubscribe<T, TH>()
+        {
+            SubsManager.RemoveSubscription<T, TH>();
+        }
+
+        private IModel CreateConsumerChannel()
+        {
+            if (!persistentConnection.isConnected)
+            {
+                persistentConnection.TryConnect();
+            }
+
+            var channel = persistentConnection.CreateModel();
+
+            channel.ExchangeDeclare(exchange: EventBusConfig.DefaultTopicName,
+                                    type: "direct");
+            return channel;
+        }
+
+        private void StartBasicConsume(string eventName)
+        {
+            if(consumerChannel is not null)
+            {
+                var consumer = new EventingBasicConsumer(consumerChannel);
+
+                consumer.Received += Consumer_Received;
+
+                consumerChannel.BasicConsume(
+                   queue: GetSubName(eventName),
+                   autoAck: false,
+                   consumer: consumer);
+            }
+        }
+
+        private async void Consumer_Received(object? sender, BasicDeliverEventArgs e)
+        {
+            var eventName = e.RoutingKey;
+            eventName = ProcessEventName(eventName);
+            var message = Encoding.UTF8.GetString(e.Body.Span);
+
+            try
+            {
+                await ProcessEvent(eventName,message);
+            }
+            catch (Exception)
+            {
+                // can be logged
+            }
+
+            consumerChannel.BasicAck(e.DeliveryTag, multiple: false);
+        }
+    }
+}
+
